cbc6ca723b5a8b6e49313019d2977f934c69856e
Saul Augusto Gasca Farrera <sgfarrera115@hotmail.com>
2021-04-27 13:14:16 -0500

    UI scene added , Skyboxes added, Alpha Faded effect script added and main UI elements added

---
+++ UserInterfaceVR/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("GameManager")]
+    public Image img;
+    public float fadeTime = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Fade",1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Fade()
+    {
+        img.CrossFadeAlpha(0, fadeTime, true);
+    }
+}
