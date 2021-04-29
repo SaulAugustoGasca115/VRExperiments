887108858740bc2ab168fa080091e7e21fb46ceb
Saul Augusto Gasca Farrera <sgfarrera115@hotmail.com>
2021-04-28 20:05:45 -0500

    UI scene added, Cupon Canvas Added, Button Normal and VR button for work with VR added

---
+++ UserInterfaceVR/Assets/Scripts/DrawDistance.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawDistance : MonoBehaviour
+{
+    [Header("Settings")]
+    [Range(0f, 10f)]
+    public float distance;
+    public Color color;
+
+    [Space]
+    [TextArea]
+    public string note;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+        Gizmos.DrawWireSphere(transform.position,distance);
+    }
+}
---
+++ UserInterfaceVR/Assets/Scripts/SendMessage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendMessage : MonoBehaviour
+{
+    public void Pointer_Enter()
+    {
+        Debug.Log("<color=green><b> " + "ENTER" + "</b></color>");
+    }
+
+    public void Pointer_Exit()
+    {
+        Debug.Log("<color=red><b> " + "EXIT" + "</b></color>");
+    }
+
+    public void Pointer_Click()
+    {
+        Debug.Log("CLICK");
+    }
+}
